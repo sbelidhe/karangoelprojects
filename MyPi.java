0319338bf01fec2f3b1124b331f6b3b28a7d5c81
sbelidhe <belidhe@hotmail.com>
2017-11-16 22:49:07 +0530

    Update and rename PI.java to MyPi.java

--- MyPi.java
+++ MyPi.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PI {
+public class MyPi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
